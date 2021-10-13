e2c59939c6b3310e4139029abc7d7ee4fb3487d7
Alex <mirag0505@yandex.ru>
2021-10-14 00:57:29 +0300

    Добавил dayjs, добавил плагин заменяющий Moment.js на dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import { Counter } from './Counter'
 import { useDispatch, useSelector } from 'react-redux'
 import { getInfoRequest } from '@src/redux/payment/state/getInfo/getInfoSlice'
 import { selectInfoList } from '@src/redux/payment/state/getInfo/getInfoSelectors'
+
 import './App.less'
 import { Button } from 'antd'
+
+import * as dayjs from 'dayjs'
+import isLeapYear from 'dayjs/plugin/isLeapYear' // import plugin
+// import plugin
+import 'dayjs/locale/zh-cn' // import locale
 
 export const App = () => {
   const dispatch = useDispatch()
@@ -23,8 +29,11 @@ export const App = () => {
         console.log('infoList', infoList)
       }
     },
-    [dispatch]
+    [dispatch, infoList]
   )
+
+  dayjs.extend(isLeapYear) // use plugin
+  dayjs.locale('zh-cn') // use locale
 
   return (
     <>
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { alias } = require('./utils/alias')
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -44,6 +45,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+    new AntdDayjsWebpackPlugin({
+      preset: 'antdv3',
     }),
   ],
   stats: 'errors-only',
